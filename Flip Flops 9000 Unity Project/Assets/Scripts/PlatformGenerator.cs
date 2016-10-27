93ee84bab9b3e84d040aa6ef88e1eac8402c1352
Preston Kelley <preston.kelley@colorado.edu>
2016-10-27 17:40:19 -0600

    Added Unity endless runner project
    Has most functionalities, can add a lot more later since I’ve been
    studying for two midterms

---
+++ Flip Flops 9000 Unity Project/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform; 
+	public Transform generationPoint;
+
+	private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+
+		platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.x < generationPoint.position.x) 
+		{
+			transform.position = new Vector3 (transform.position.x + platformWidth + 3, transform.position.y, transform.position.z);
+
+			Instantiate (thePlatform, transform.position, transform.rotation);
+		}
+	
+	}
+}
