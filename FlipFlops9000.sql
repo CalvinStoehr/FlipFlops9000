fbcca8a4366cf014f9b3c7085654d8f79fa01673
codo8769 <codo8769@users.noreply.github.com>
2016-10-31 16:33:52 -0600

    Add files via upload

---
+++ FlipFlops9000.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE FlipFlops;
+USE FlipFlops;
+CREATE TABLE UserData(userid float, username char(30), password char(30), datejoined date, devicetype char(10));
+PRIMARY KEY ('userId');
+INSERT INTO UserData VALUES (1, 'differentstate4', 'password123', '2016-10-30', 'ios');
+INSERT INTO UserData VALUES (2, 'redhouse91', 'qwertyuiop', '2016-10-31', 'android');
+INSERT INTO UserData VALUES (3, 'longstudent16', 'qazwsxedcr', '2016-11-2', 'ios');
+INSERT INTO UserData VALUES (4, 'littlegroup24', 'asdfghjkl', '2017-1-5', 2, 'pc');
+INSERT INTO UserData VALUES (5, 'importantcountry29', 'il!kefr33dom', '1776-7-4', 'eagle'); 
+
+
+create table userStats(username char(30), highest_score integer, total_runs integer, bosses_beaten integer, boss_encounters integer);
+insert into userStats values ('differentstate4', 2048, 10, 1, 2);
+insert into userStats values ('redhouse91', 4167, 28, 2, 2);
+insert into userStats values ('longstudent16', 7283, 36, 3, 4);
+insert into userStats values ('littlegroup24', 4864, 16, 2, 3);
+insert into userStats values ('importantcountry29', 3245, 12, 2, 2);
+
+CREATE TABLE BossData(username char(30), boss1encounter float, boss1beat float, boss2encounter float, boss2beat float);
+INSERT INTO BossData VALUES ('differentstate4', 12, 4, 3, 0);
+INSERT INTO BossData VALUES ('redhouse91', 100, 75, 50, 25);
+INSERT INTO BossData VALUES ('longstudent16', 37, 4, 2, 9);
+INSERT INTO BossData VALUES ('littlegroup24', 22, 20, 12, 2);
+INSERT INTO BossData VALUES ('importantcountry29', 100, 100, 100, 100);
