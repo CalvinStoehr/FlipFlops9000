84eb3822ec25640c77b15250c4b2b4cbd6efeaf8
CollinDornbos <codo8769@colorado.edu>
2016-11-04 13:15:49 -0600

    Added main menu and settings menu. All buttons work except 'Leaderboard'.

---
+++ Flip Flops 9000 Unity Project/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+	public void ChangeSceneButton(int newScene)
+	{
+		SceneManager.LoadScene (newScene);
+	}
+
+	public void ExitGameButton()
+	{
+		Application.Quit ();
+	}
+
+	public void MasterVolumeSlider(float vol)
+	{
+		AudioListener.volume = vol;
+	}
+
+}
