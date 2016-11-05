88e40ec6909111b704dd7a0608cd7bc9461ec0d5
Jeff Everett <everett.jeff.w@gmail.com>
2016-11-05 04:19:59 -0600

    Added new project with character animations

---
+++ RobotDude/Assets/Calvin_Stand_In_Assets/Scripts/Background Scripts/BGLooper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooper : MonoBehaviour {
+
+	[SerializeField] private GameObject player;
+	private PlatformerCharacter2D playerScript;
+
+    public float speed = 0.1f;
+
+    private Vector2 offset = Vector2.zero;
+    private Material mat;
+    
+    // Use this for initialization
+	void Start ()
+	{
+		playerScript = player.GetComponent<PlatformerCharacter2D> ();
+        mat = GetComponent<Renderer>().material;
+        offset = mat.GetTextureOffset("_MainTex");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (playerScript.m_Dead)
+			this.enabled = false;
+		
+        offset.x += speed * Time.deltaTime;
+        mat.SetTextureOffset("_MainTex", offset);
+	}
+}
---
+++ RobotDude/Assets/Scripts/InteractablesSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractablesSpawner : MonoBehaviour {
+
+	[SerializeField] private GameObject player;
+	private PlatformerCharacter2D playerScript;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerScript = player.GetComponent<PlatformerCharacter2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerScript.m_Dead)
+			this.enabled = false;
+		
+		for (int i = 0; i < transform.childCount; i++) {
+			transform.GetChild (i).transform.position -= new Vector3(3f, 0f, 0f) * Time.deltaTime;
+		}
+	}
+}
